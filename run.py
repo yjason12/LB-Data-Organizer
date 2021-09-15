350b8e0f099be94e7bbb015307269553c65b8b16
Jason Lee <jyl015@ucsd.edu>
2021-09-14 17:54:55 -0700

    fav genre method
    created working fav genre method

--- run.py
+++ run.py
@@ -1,7 +1,4 @@
 import calcmethods
 
 username =  "yongjaeson"
-genre = "horror"
-dramaAvg = calcmethods.calc_genre_avg(genre, username)
-
-print(username + "\'s average rating for " + genre + " is " + str(dramaAvg))
+dramaAvg = calcmethods.fav_genre(username)
